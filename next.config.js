ae21e03ea9b20552df465e50cfd7a7e11cdc0a58
ThatNinjaGuy <vishalgautam.dtu@gmail.com>
2024-01-13 18:30:17 +0530

    pushing config files

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Allows application to access the links with the below hotnames
+  images: {
+    domains: ["lh3.googleusercontent.com", "images.unsplash.com"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  theme: {
+    extend: {
+      // Setting the primary, and secondary colors
+      colors: {
+        primary: "#038C7F",
+        secondary: "#F2C641",
+        // Different tertiary colors for light and dark theme
+        tertiary: {
+          dark: "#F27405",
+          light: "#F2C641",
+        },
+      },
+      // Setting font family for the complete application
+      fontFamily: {
+        poppins: ["var(--font-poppins)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
